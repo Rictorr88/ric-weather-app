043db8788c983dbd9016049b2dc0d3658511abb4
Daniel Lopez <neo_19_8@hotmail.com>
2023-09-28 16:26:51 -0600

    working on format-data.js

--- js/current-weather.js
+++ js/current-weather.js
@@ -1,19 +1,38 @@
 import weather from '../data/current-weather.js'
+import { formatDate, formatTemp } from "./utils/format-data.js";
 // weather.name
 
 function setCurrentCity($el, city) {
     $el.textContent = city
 }
 
+
+function setCurrentDate($el) {
+    const date = new Date ()
+    const formattedDate = formatDate(date)
+    $el.textContent = formattedDate
+}
+
+function setCurrentTemp($el, temp) {
+    $el.textContent = formatTemp(temp)
+}
+
 function configCurrentWeather(weather) {
 
 // loader    
 //date
+const $currentWeatherDate = document.querySelector('#current-weather-date')
+setCurrentDate($currentWeatherDate)
 //city    
 const $currentWeatherCity = document.querySelector('#current-weather-city')
 const city = weather.name
 setCurrentCity($currentWeatherCity, city)
 // temp  
+const $currentWeatherTemp = document.querySelector('#current-weather-temp')
+const temp = weather.main.temp
+setCurrentTemp($currentWeatherTemp, temp)
+
+
 //background    
     
 }
@@ -21,5 +40,6 @@ setCurrentCity($currentWeatherCity, city)
 export default function currentWeather() {
     //GEO // API // -weather // config
     configCurrentWeather(weather)
-    console.log('weather')
-}
+    console.log(weather)
+} 
+
