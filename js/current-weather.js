77b32a402477eb2dfcab60f35c2abbf63ea2eff4
Daniel Lopez <neo_19_8@hotmail.com>
2023-10-03 19:22:19 -0600

    project fix

--- js/current-weather.js
+++ js/current-weather.js
@@ -38,10 +38,17 @@ function setBackground($el, conditionCode, solarStatus) {
     $el.style.backgroundImage = `url(./images/${solarStatus}-${weatherType}${size}.jpg` 
 }
 
+function showCurrentWeather($app, $loader) {
+    $app.hidden = false 
+    $loader.hidden = true
+}
 
 function configCurrentWeather(weather) {
+    const $app = document.querySelector("#app")
+    const $loading = document.querySelector("#loading")
 
 // loader    
+showCurrentWeather($app, $loading)
 //date
 const $currentWeatherDate = document.querySelector('#current-weather-date')
 setCurrentDate($currentWeatherDate)
@@ -58,7 +65,6 @@ setCurrentTemp($currentWeatherTemp, temp)
 //background    
 const sunriseTime = new Date(weather.sys.sunrise * 1000)
 const sunsetTime = new Date(weather.sys.sunset * 1000)
-const $app = document.querySelector('#app')
 const conditionCode = String(weather.weather[0].id).charAt(0)
 setBackground($app, conditionCode, solarStatus(sunriseTime, sunsetTime))
 
